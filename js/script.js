1c473ab43a6c8e1113a63b0b1fafaae250f15253
Dion2011q <dion.p.dogger@protonmail.com>
2025-07-13 12:50:58 +0200

    v5

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // year footer
 const yearEl = document.getElementById('year');
 if (yearEl) yearEl.textContent = new Date().getFullYear();
+
+// age calculation
+const birthYear = 2011;
+const ageEl = document.getElementById('age');
+if (ageEl) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+    ageEl.textContent = age;
+}
